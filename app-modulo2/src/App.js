868f66fa4fc4213001b89a8adcf5475e7856a03e
João Pedro <jpmuniz88@gmail.com>
2018-12-12 23:46:43 -0200

    inicia modulo2

---
+++ app-modulo2/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Inicio modulo2</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
